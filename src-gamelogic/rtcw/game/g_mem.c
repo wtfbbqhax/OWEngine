2ddd04c00ff6158f88b13bd182730c810796a709
Dusan Jocic <dusanjocic@msn.com>
2016-11-22 18:09:55 +0100

    Increased POOLSIZE for G_Alloc so maps can load

--- src-gamelogic/rtcw/game/g_mem.c
+++ src-gamelogic/rtcw/game/g_mem.c
@@ -40,13 +40,8 @@
 //
 //////////////////////////////////////////////////////////////////////////////////////
 
-
 #include "g_local.h"
-
-// Ridah, increased this (fixes Dan's crash)
-//#define POOLSIZE	(256 * 1024)
-//#define POOLSIZE	(2048 * 1024)
-#define POOLSIZE    ( 4096 * 1024 )   //----(SA)	upped to try to get assault_34 going
+#define POOLSIZE ( 8192 * 1024 )   //Dushan
 
 static char memoryPool[POOLSIZE];
 static int allocPoint;
